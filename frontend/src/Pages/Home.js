2c486cf3f0bb0c23e2dc1a03d1bc83e8162e0875
ThunderStorm24 <patryko5@onet.pl>
2023-10-11 11:12:02 +0200

    Add folders

---
+++ frontend/src/Pages/Home.js
@@ -0,0 +1,14 @@
+
+import React, { useState } from 'react';
+import MyModal from '../Components/LoginModal.js';
+
+
+function Home() {
+  return (
+    <div className="App">
+        <MyModal />
+    </div>
+  );
+}
+
+export default Home;
