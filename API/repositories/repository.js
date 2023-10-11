b20fd23f8da93b8d189b55fda9c8a059afeda42c
Jakub Bałda <balda.jakub00@gmail.com>
2023-10-11 17:03:54 +0200

    Developed login use case, added database queries for login

--- API/repositories/repository.js
+++ API/repositories/repository.js
@@ -1,7 +1,27 @@
 const database = require('../database');
 
-async function getAllItems(){
+async function getLoginData(userLogin){
+    let query = `SELECT id, user, password, role, isBanned FROM user WHERE user = '${userLogin}'`;
 
+    try{
+        let userData = await database.sqlQuery(query);
+
+        return userData;
+    }catch(err){
+        throw err;
+    }
 }
 
-module.exports = {getAllItems}
+async function getUserRole(roleId){
+    let query = `SELECT name FROM roles WHERE id = ${roleId}`;
+
+    try{
+        let userRole = await database.sqlQuery(query);
+
+        return userRole;
+    }catch(err){
+        throw err;
+    }
+}
+
+module.exports = {getLoginData, getUserRole}
--- API/services/loginUserUseCase.js
+++ API/services/loginUserUseCase.js
@@ -1,5 +1,63 @@
+const repository = require('../repositories/repository');
+
 async function login(userData){
-    return 'xD';
+    let dbUserData;
+
+    try{
+        dbUserData = await repository.getLoginData(userData.login);
+    }catch(err){
+        throw err;
+    }
+
+    if(dbUserData !== undefined){
+
+        if(isUserBanned(dbUserData[0].isBanned)){
+            return {loginError: false, passwordError: false, dbError: false, userBanned: true};
+        }
+
+        if(compareLoginData(dbUserData[0], userData)){
+            let userRole = await repository.getUserRole(dbUserData[0].role);
+            
+            if(userRole[0] !== undefined)
+                return {userId: dbUserData[0].id, login: dbUserData[0].user, role: userRole[0].name};
+            else
+                return {loginError: false, passwordError: false, dbError: true, userBanned: false};
+        }else{
+            return {loginError: true, passwordError: true, dbError: false, userBanned: false};
+        }
+    }
+    
+    return {loginError: true, passwordError: false, dbError: false, userBanned: false};
+}
+
+function compareLoginData(dbUserData, userData){
+    let loginCompleated = true;
+
+    if(dbUserData !== undefined){
+
+        if(dbUserData.user !== userData.login){
+            loginCompleated = false;
+        }
+
+        if(dbUserData.password !== userData.password){
+            loginCompleated = false;
+        }
+
+    }else{
+        loginCompleated = false;
+    }
+
+    return loginCompleated;
+}
+
+function isUserBanned(userBanStatus){
+    let isBanned = false;
+
+    if(userBanStatus === 1){
+        isBanned = true;
+    }
+
+    return isBanned;
 }
 
 module.exports = {login}
