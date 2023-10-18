76205339ca7d4c49c33c496cfc7d69abcc797d7c
Samessprog <100785560+Samessprog@users.noreply.github.com>
2023-10-18 15:07:03 +0200

    baw sie thunder ( Fetch searching data ) Co-authored-by: Thunder <ThunderStorm24@users.noreply.github.com>

--- frontend/src/Components/SearchOptionsModal.js
+++ frontend/src/Components/SearchOptionsModal.js
@@ -1,33 +1,34 @@
-import { useState, useReducer } from "react";
+import { useReducer } from "react";
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import { catalogChildrenTree, catalogs, colors, materials, status, brands, VideoGameRatings } from '../lib/lib'
 
 
 function SearchOptionsModal({ searchingState, onHideSearchOptionsModal, showSearchOptionsModal }) {
+
 
     //Searching Reducer *ZMIENIA STANY JAK COŚ*
     const searchingReducer = (searchingStates, action) => {
         switch (action.type) {
             case 'CHANGE_SELECTED_COLOR':
-                return { ...searchingStates, selectedColor: action.newState }
+                return { ...searchingStates, colorIDs: action.newState }
             case 'CHANGE_SELECTED_MATERIAL':
-                return { ...searchingStates, selectedMaterial: action.newState }
+                return { ...searchingStates, materialIDs: action.newState }
             case 'CHANGE_SELECTED_ITEMSTATUS':
-                return { ...searchingStates, selectedItemStatus: action.newState }
+                return { ...searchingStates, statusIDs: action.newState }
             case 'CHANGE_SELECTED_CATALOGUE':
-                return { ...searchingStates, selectedCatalogue: action.newState }
-            case 'CHANGE_SELECTED_SIZE':
-                return { ...searchingStates, selectedSize: action.newState }
+                return { ...searchingStates, catalogIDs: action.newState }
             case 'CHANGE_SELECTED_VIDEOGAMERATING':
-                return { ...searchingStates, selectedVideoGameRating: action.newState }
+                return { ...searchingStates, videoGameRatingIDs: action.newState }
             case 'CHANGE_SELECTED_BRAND':
-                return { ...searchingStates, selectedBrand: action.newState }
+                return { ...searchingStates, brandIDs: action.newState }
             default:
                 return
         }
     }
 
     const [searchingStates, dispatch] = useReducer(searchingReducer, searchingState)
+    console.log(searchingStates)
 
     return (
 
@@ -45,45 +46,52 @@ function SearchOptionsModal({ searchingState, onHideSearchOptionsModal, showSear
             </Modal.Header>
             <Modal.Body className="d-flex justify-content-center">
                 <div className="col-10 d-flex flex-column">
-                    <select value={searchingStates.selectedColor} onChange={(e) => dispatch({ type: 'CHANGE_SELECTED_COLOR', newState: e.target.value })} className="form-select ms-2 mb-3 mt-3" id="inputGroupSelect01">
-                        <option >Select a color</option>
-                        <option>Niebieski</option>
-                        <option>Czerwony</option>
-                        {/* MORE OPTIONS */}
+                    <select value={searchingStates.colorIDs} onChange={(e) => dispatch({ type: 'CHANGE_SELECTED_COLOR', newState: e.target.value })} className="form-select ms-2 mb-3 mt-3" id="inputGroupSelect01">
+                        <option>Select a color</option>
+                        {colors.map(color => (
+                            <option key={color.id} value={color.id}>{color.title}</option>
+                        ))}
                     </select>
-                    <select value={searchingStates.selectedMaterial} onChange={(e) => dispatch({ type: 'CHANGE_SELECTED_MATERIAL', newState: e.target.value })} className="form-select ms-2 mb-3" id="inputGroupSelect01">
+                    <select value={searchingStates.materialIDs} onChange={(e) => dispatch({ type: 'CHANGE_SELECTED_MATERIAL', newState: e.target.value })} className="form-select ms-2 mb-3" id="inputGroupSelect01">
                         <option>Select a material</option>
-                        <option>Jedwab</option>
+                        {materials.map(material => (
+                            <option key={material.id} value={material.id}>{material.title}</option>
+                        ))}
                     </select>
-                    <select value={searchingStates.selectedItemStatus} onChange={(e) => dispatch({ type: 'CHANGE_SELECTED_ITEMSTATUS', newState: e.target.value })} className="form-select ms-2 mb-3" id="inputGroupSelect01">
+                    <select value={searchingStates.statusIDs} onChange={(e) => dispatch({ type: 'CHANGE_SELECTED_ITEMSTATUS', newState: e.target.value })} className="form-select ms-2 mb-3" id="inputGroupSelect01">
                         <option>Select item status</option>
-                        <option>Status</option>
+                        {status.map(stat => (
+                            <option key={stat.id} value={stat.id}>{stat.title}</option>
+                        ))}
                     </select>
-                    <select value={searchingStates.selectedCatalogue} onChange={(e) => dispatch({ type: 'CHANGE_SELECTED_CATALOGUE', newState: e.target.value })} className="form-select ms-2 mb-3" id="inputGroupSelect01">
+                    <select value={searchingStates.catalogIDs} onChange={(e) => dispatch({ type: 'CHANGE_SELECTED_CATALOGUE', newState: e.target.value })} className="form-select ms-2 mb-3" id="inputGroupSelect01">
                         <option>Select a catalogue</option>
-                        <option>Katalog</option>
+                        {Object.entries(catalogChildrenTree).map(([id, group]) => (
+                            <optgroup label={catalogs[id]?.title} key={id}>
+                                {group.map(entryId => (
+                                    <option value={entryId} key={entryId}>
+                                        {catalogs[entryId]?.title}
+                                    </option>
+                                ))}
+                            </optgroup>
+                        ))}
                     </select>
-                    <select value={searchingStates.selectedSize} onChange={(e) => dispatch({ type: 'CHANGE_SELECTED_SIZE', newState: e.target.value })} className="form-select ms-2 mb-3" id="inputGroupSelect01">
-                        <option>Select a size</option>
-                        <option>baldzik</option>
-                        <option>small</option>
-                        <option>medium</option>
-                        <option>big</option>
-                        <option>giant</option>
-                    </select>
-                    <select value={searchingStates.selectedVideoGameRating} onChange={(e) => dispatch({ type: 'CHANGE_SELECTED_VIDEOGAMERATING', newState: e.target.value })} className="form-select ms-2 mb-3" id="inputGroupSelect01">
+                    <select value={searchingStates.videoGameRatingIDs} onChange={(e) => dispatch({ type: 'CHANGE_SELECTED_VIDEOGAMERATING', newState: e.target.value })} className="form-select ms-2 mb-3" id="inputGroupSelect01">
                         <option>Select a videoGameRating</option>
-                        <option>10/10</option>
+                        {VideoGameRatings.map(VideoGameRating => (
+                            <option key={VideoGameRating.id} value={VideoGameRating.id}>{VideoGameRating.title}</option>
+                        ))}
                     </select>
-                    <select value={searchingStates.selectedBrand} onChange={(e) => dispatch({ type: 'CHANGE_SELECTED_BRAND', newState: e.target.value })} className="form-select ms-2 mb-3" id="inputGroupSelect01">
+                    <select value={searchingStates.brandIDs} onChange={(e) => dispatch({ type: 'CHANGE_SELECTED_BRAND', newState: e.target.value })} className="form-select ms-2 mb-3" id="inputGroupSelect01">
                         <option>Select a brand</option>
-                        <option>Ford</option>
-                        <option>Mercedes</option>
+                        {brands.map(brand => (
+                            <option key={brand.id} value={brand.id}>{brand.title}</option>
+                        ))}
                     </select>
                 </div>
             </Modal.Body>
             <Modal.Footer>
-                <Button>Close</Button>
+                <Button>Save</Button>
             </Modal.Footer>
         </Modal>
 
