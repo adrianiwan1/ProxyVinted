f5c7c952119b6fcc1ae94ebccea8cace177f89b9
Jakub Bałda <balda.jakub00@gmail.com>
2023-11-07 23:41:44 +0100

    Fixed cookie fetching

--- API/services/getAllItemsUseCase.js
+++ API/services/getAllItemsUseCase.js
@@ -20,8 +20,8 @@ async function vintedSearch (searchParams) {
   const c = await fetchCookie('pl')
   const response = await fetch(parseVintedURL(searchParams), {
     headers: {
-      'user-agent': random(),
-      cookie: '_vinted_fr_session=' + c,
+      'User-Agent': random(),
+      cookie: c,
       accept: 'application/json, text/plain, */*'
     }
   })
@@ -33,8 +33,9 @@ async function fetchCookie (domain = 'pl') {
   const response = await fetch(`https://vinted.${domain}`)
   const sessionCookie = response.headers.get('set-cookie')
   if (!sessionCookie) throw new Error('Session cookie not found')
-  const c = cookie.parse(sessionCookie)['secure, _vinted_fr_session']
-  return c
+  let cookies = sessionCookie.split(';')
+  const cookieSesion = cookies[11].split(', ')
+  return cookieSesion[1]
 }
 
 module.exports = { getAllItems }
