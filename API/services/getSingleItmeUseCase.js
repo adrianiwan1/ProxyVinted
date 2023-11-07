ab52c12219a1a120e33c57d84d2c4d78fa5ba894
Jakub Bałda <balda.jakub00@gmail.com>
2023-11-07 23:51:15 +0100

    Fixed cookie parsing in single item use case

--- API/services/getSingleItmeUseCase.js
+++ API/services/getSingleItmeUseCase.js
@@ -15,7 +15,7 @@ async function vintedSearch (searchParams) {
   const response = await fetch(parseVintedURL(searchParams), {
     headers: {
       'user-agent': random(),
-      cookie: '_vinted_fr_session=' + c,
+      cookie: c,
       accept: 'application/json, text/plain, */*'
     }
   })
@@ -27,8 +27,9 @@ async function fetchCookie (domain = 'pl') {
   const response = await fetch(`https://vinted.${domain}`)
   const sessionCookie = response.headers.get('set-cookie')
   if (!sessionCookie) throw new Error('Session cookie not found')
-  const c = cookie.parse(sessionCookie)['secure, _vinted_fr_session']
-  return c
+  let cookies = sessionCookie.split(';')
+  const cookieSesion = cookies[11].split(', ')
+  return cookieSesion[1]
 }
 
 module.exports = { getItem }
